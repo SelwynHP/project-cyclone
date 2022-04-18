02e5a096decc6bef1a4c3509834127661b7f6de8
Selwyn Horne-Primus <selwyn_anthonyhp@live.ca>
2022-04-18 14:53:04 -0400

    Fix: Merchant Login and validation

--- client/src/components/MerchantCredentials/MerchantLogin.js
+++ client/src/components/MerchantCredentials/MerchantLogin.js
@@ -4,62 +4,83 @@ import {formType, ValidateNewMerchant} from "../../validation/validation-utils";
 import {auth} from "../Firebase";
 import {deleteUser} from "firebase/auth";
 import FormError from "../FormError";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {firebaseErrorCodes, formErrorCodes} from "../../validation/error-codes";
 
 const MerchantLogin = ({type}) => {
     const [error, setError] = useState(null);
-    const handleMerchantSignup = async () => {
+
+    const handleMerchantLogin = async (ev) => {
+        ev.preventDefault();
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
         const merchant = {
-            email: email,
-            merchantName: document.getElementById("merchant-name").value,
-            telephone: document.getElementById("telephone").value,
-            bAddress: document.getElementById("billing-address").value,
-            sAddress: document.getElementById("shipping-address").value,
-            pNotes: document.getElementById("pickup-notes").value
+            email,
+            merchantName: document.getElementById("merchant-name")?.value,
+            telephone: document.getElementById("telephone")?.value,
+            bAddress: document.getElementById("billing-address")?.value,
+            sAddress: document.getElementById("shipping-address")?.value,
+            pNotes: document.getElementById("pickup-notes")?.value
         }
-
-        if (ValidateNewMerchant(merchant)) {
-            await formType[type].buttonFunction(auth, email, password)
-                .then(() => {
-                    return fetch("/api/create-merchant", {
-                        method: "POST",
-                        body: JSON.stringify(merchant),
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                    })
-                        .then(res => res.json())
-                        .then(data => {
-                            return data;
-                        })
-                        .catch(err => {
-                            console.log(err.message);
-                            deleteUser(auth.currentUser);
-                        })
-                })
-                .catch(err => {
-                    setError(err.message);
-                })
+        console.log(merchant);
+        //Validation
+        if (!email) {
+            setError(formErrorCodes["email"])
+        } else if (!password) {
+            setError(formErrorCodes["password"])
+        }
+        if (email && password) {
+            if (type === "mSignup") {
+                if (ValidateNewMerchant(merchant)) {
+                    console.log("valid");
+                    try {
+                        const result = await formType[type].buttonFunction(auth, email, password);
+                        console.log(result);
+                        if (result) {
+                            await fetch("/api/create-merchant", {
+                                method: "POST",
+                                body: JSON.stringify(merchant),
+                                headers: {
+                                    "Content-Type": "application/json",
+                                },
+                            })
+                        }
+                    } catch (err) {
+                        await deleteUser(auth.currentUser);
+                        setError(err.message());
+                    }
+                }
+            } else if (type === "mLogin") {
+                try {
+                    await formType[type].buttonFunction(auth, email, password);
+                } catch (err) {
+                    console.log(err.code);
+                    setError(firebaseErrorCodes[err.code] ?? firebaseErrorCodes["unknown"]);
+                }
+            }
         }
     }
+
+    useEffect(() => {
+        setError(null);
+    }, [type]);
+
     return (
         <Wrapper>
             <h1>{formType[type].formTitle}</h1>
             <form>
-                <input id="email" type="text" placeholder="Email"/>
+                <input id="email" type="email" placeholder="Email"/>
                 <input id="password" type="password" placeholder="Password"/>
                 {type === "mSignup" &&
                     <>
                         <input id="merchant-name" type="text" placeholder="Company Name"/>
-                        <input id="telephone" type="text" placeholder="Telephone"/>
+                        <input id="telephone" type="tel" placeholder="Telephone"/>
                         <input id="billing-address" type="text" placeholder="Billing Address"/>
                         <input id="shipping-address" type="text" placeholder="Shipping Address"/>
                         <input id="pickup-notes" type="text" placeholder="Pickup notes"/>
                     </>
                 }
-                <button onClick={(ev) => handleMerchantSignup(ev)}>{formType[type].buttonTitle}</button>
+                <button onClick={(ev) => handleMerchantLogin(ev)}>{formType[type].buttonTitle}</button>
                 {error &&
                     <FormError error={error}/>}
             </form>
