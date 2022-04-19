d2b1725d312436eac991dbb8534644b32e53e7e9
Selwyn Horne-Primus <selwyn_anthonyhp@live.ca>
2022-04-19 14:41:59 -0400

    Added Maps to TrackingDetails.js

--- client/src/components/Tracking/TrackingDetails.js
+++ client/src/components/Tracking/TrackingDetails.js
@@ -1,19 +1,30 @@
-import styled from "styled-components";
-import {useParams} from "react-router-dom";
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 
 const TrackingDetails = () => {
-    const {orderId} = useParams();
+    const containerStyle = {
+        width: '800px',
+        height: '400px'
+    };
+
+    const center = {
+        lat: 45.508888,
+        lng: -73.561668
+    };
+
+    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
     return (
-        <Wrapper>
-            <h1>Tracking Order# {orderId}</h1>
-            <div></div>
-        </Wrapper>
+        <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        >
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={10}
+            >
+                { /* Child components, such as markers, info windows, etc. */}
+                <></>
+            </GoogleMap>
+        </LoadScript>
     )
 }
-
-const Wrapper = styled.div`
-  & div {
-    background-color: aqua;
-  }
-`
 export default TrackingDetails;
